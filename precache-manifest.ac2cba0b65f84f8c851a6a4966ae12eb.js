91f452cb4dc98f13c0425635f2ecf157d5eb4731
amous4822 <amous4822@gmail.com>
2019-06-23 12:27:46 +0530

    Updates

---
+++ precache-manifest.ac2cba0b65f84f8c851a6a4966ae12eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "847df103da4b74cc84af",
+    "url": "/test-deploy/static/js/runtime~main.90bc13d1.js"
+  },
+  {
+    "revision": "86de34152716383d144b",
+    "url": "/test-deploy/static/js/main.7b1255a8.chunk.js"
+  },
+  {
+    "revision": "41fcae9c23614894dfcf",
+    "url": "/test-deploy/static/js/2.db40d1a9.chunk.js"
+  },
+  {
+    "revision": "86de34152716383d144b",
+    "url": "/test-deploy/static/css/main.cd4804a7.chunk.css"
+  },
+  {
+    "revision": "41fcae9c23614894dfcf",
+    "url": "/test-deploy/static/css/2.d5e0d0ff.chunk.css"
+  },
+  {
+    "revision": "600c347a481673e7f0c7cbc972fd4ce0",
+    "url": "/test-deploy/index.html"
+  }
+];
